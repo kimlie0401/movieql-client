f29f6c22a9ad734b91b317fe008c2b0d81112524
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2019-10-18 11:53:39 -0700

    Change font

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,10 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 export const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css?family=Maven+Pro:400,500');
+@import url('https://fonts.googleapis.com/css?family=Indie+Flower&display=swap');
   ${reset};
   body{
-    font-family: 'Maven Pro', sans-serif;
+    font-family: 'Indie Flower', cursive;
     background-color: #2f2f2f;
     color: white;
   }
